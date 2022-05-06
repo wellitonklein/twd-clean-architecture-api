1b68683cd9e71111c566c4057b41d2c97030d204
Welliton Klein <welliton.fokushima@gmail.com>
2022-05-06 12:28:11 -0300

    refactor: improve variable name in test

--- test/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
+++ test/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
@@ -5,8 +5,8 @@ describe('In memory User repository', () => {
   test('should return null if user is not foud', async () => {
     const users: UserData[] = []
     const email = 'any@mail.com'
-    const userRepo = new InMemoryUserRepository(users)
-    const user = await userRepo.findUserByEmail(email)
+    const sut = new InMemoryUserRepository(users)
+    const user = await sut.findUserByEmail(email)
     expect(user).toBeNull()
   })
 
@@ -14,9 +14,9 @@ describe('In memory User repository', () => {
     const users: UserData[] = []
     const name = 'any_name'
     const email = 'any@mail.com'
-    const userRepo = new InMemoryUserRepository(users)
-    await userRepo.add({ name, email })
-    const user = await userRepo.findUserByEmail(email)
+    const sut = new InMemoryUserRepository(users)
+    await sut.add({ name, email })
+    const user = await sut.findUserByEmail(email)
     expect(user.name).toBe(name)
   })
 
@@ -25,8 +25,8 @@ describe('In memory User repository', () => {
       { name: 'any_name', email: 'any@mail.com' },
       { name: 'second_name', email: 'second@mail.com' }
     ]
-    const userRepo = new InMemoryUserRepository(users)
-    const returnedUsers = await userRepo.findAllUsers()
+    const sut = new InMemoryUserRepository(users)
+    const returnedUsers = await sut.findAllUsers()
     expect(returnedUsers.length).toBe(2)
   })
 })
