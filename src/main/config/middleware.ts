637da13731b647e9b457bb3e59ad2af254021147
Welliton Klein <welliton.fokushima@gmail.com>
2022-05-06 19:26:44 -0300

    feat: apply content type middleware

--- src/main/config/middleware.ts
+++ src/main/config/middleware.ts
@@ -1,8 +1,9 @@
 import { Express } from 'express'
 
-import { bodyParser, cors } from '@/main/config/middleware/'
+import { bodyParser, cors, contentType } from '@/main/config/middleware/'
 
 export default (app: Express): void => {
   app.use(bodyParser)
   app.use(cors)
+  app.use(contentType)
 }
