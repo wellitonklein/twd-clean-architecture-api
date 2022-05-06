a9dc0a609c458cf962430c33a195966bf4f5ee3a
Welliton Klein <welliton.fokushima@gmail.com>
2022-05-06 11:07:35 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,28 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+module.exports = {
+  // A list of paths to directories that Jest should use to search for files in
+  roots: [
+    '<rootDir>/src'
+  ],
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: 'coverage',
+
+  // An array of glob patterns indicating a set of files for which coverage information should be collected
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts'
+  ],
+
+  // The test environment that will be used for testing
+  testEnvironment: 'node',
+
+  // A map from regular expressions to paths to transformers
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+
+}
