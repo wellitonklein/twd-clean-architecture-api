0984b5faa341e8be7e65187e82428dc55c0af885
Welliton Klein <welliton.fokushima@gmail.com>
2022-05-06 12:26:04 -0300

    test: return all users in the repository

--- test/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
+++ test/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
@@ -19,4 +19,14 @@ describe('In memory User repository', () => {
     const user = await userRepo.findUserByEmail(email)
     expect(user.name).toBe(name)
   })
+
+  test('should return all users in the repository', async () => {
+    const users: UserData[] = [
+      { name: 'any_name', email: 'any@mail.com' },
+      { name: 'second_name', email: 'second@mail.com' }
+    ]
+    const userRepo = new InMemoryUserRepository(users)
+    const returnedUsers = await userRepo.findAllUsers()
+    expect(returnedUsers.length).toBe(2)
+  })
 })
