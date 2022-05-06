b9e1230f0cc56867e7cc8c63f3c52e1b486d5360
Welliton Klein <welliton.fokushima@gmail.com>
2022-05-06 18:27:06 -0300

    feat: web controller should return 400 when request is missing parameters

--- src/web-controllers/register-user-controller.ts
+++ src/web-controllers/register-user-controller.ts
@@ -1,7 +1,8 @@
 import { UserData } from '@/entities'
 import { RegisterUserOnMailingList } from '@/usecases/register-user-on-mailing-list'
 import { HttpRequest, HttpResponse } from '@/web-controllers/ports'
-import { created } from '@/web-controllers/util'
+import { badRequest, created } from '@/web-controllers/util'
+import { MissingParamError } from '@/web-controllers/errors'
 
 export class RegisterUserController {
   private readonly usecase: RegisterUserOnMailingList
@@ -11,8 +12,25 @@ export class RegisterUserController {
   }
 
   async handle (request: HttpRequest): Promise<HttpResponse> {
+    if (!request.body.name || !request.body.email) {
+      let missingParam: string
+
+      if (!request.body.name && !request.body.email) {
+        missingParam = 'name and email'
+      } else {
+        missingParam = !(request.body.name) ? 'name' : ''
+        missingParam += !(request.body.email) ? 'email' : ''
+      }
+
+      return badRequest(new MissingParamError(missingParam))
+    }
+
     const userData = request.body as UserData
     const response = await this.usecase.perform(userData)
+
+    if (response.isLeft()) {
+      return badRequest(response.value)
+    }
 
     if (response.isRight()) {
       return created(response.value)
