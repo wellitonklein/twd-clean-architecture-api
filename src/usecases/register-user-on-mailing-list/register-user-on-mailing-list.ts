f3ef973dbb0481f706700cfffb2e982352ce2cc2
Welliton Klein <welliton.fokushima@gmail.com>
2022-05-06 16:21:59 -0300

    feat: register valid user to mailing list

---
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.ts
@@ -0,0 +1,28 @@
+import { InvalidEmailError } from '../../entities/errors/invalid-email-error'
+import { InvalidNameError } from '../../entities/errors/invalid-name-error'
+import { User } from '../../entities/user'
+import { UserData } from '../../entities/user-data'
+import { Either, left, right } from '../../shared/either'
+import { UserRepository } from '../ports/user-repository'
+
+export class RegisterUserOnMailingList {
+  private userRepo: UserRepository
+
+  constructor (userRepo: UserRepository) {
+    this.userRepo = userRepo
+  }
+
+  async perform (request: UserData): Promise<Either<InvalidNameError | InvalidEmailError, UserData>> {
+    const errorOrUser = User.create(request)
+
+    if (errorOrUser.isLeft()) {
+      return left(errorOrUser.value)
+    }
+
+    if (!(await this.userRepo.exists(request))) {
+      await this.userRepo.add(request)
+    }
+
+    return right(request)
+  }
+}
