f2bdeb529fcdd51d3775212eff16f8cd23b949e6
Welliton Klein <welliton.fokushima@gmail.com>
2022-05-08 12:42:05 -0300

    chore: configure jest mongodb

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,8 @@
  */
 
 module.exports = {
+  present: '@shelf/jest-mongodb',
+
   // A list of paths to directories that Jest should use to search for files in
   roots: [
     '<rootDir>/test'
@@ -18,9 +20,6 @@ module.exports = {
     '!**/test/**',
     '!**/config/*'
   ],
-
-  // The test environment that will be used for testing
-  testEnvironment: 'node',
 
   // A map from regular expressions to paths to transformers
   transform: {
