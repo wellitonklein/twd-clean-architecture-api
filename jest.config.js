1ed70b7556c08913db2d6c0f2fae2a48c2f28657
Welliton Klein <welliton.fokushima@gmail.com>
2022-05-08 13:22:19 -0300

    chore: configure jest mongodb

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@
  */
 
 module.exports = {
-  present: '@shelf/jest-mongodb',
+  preset: '@shelf/jest-mongodb',
 
   // A list of paths to directories that Jest should use to search for files in
   roots: [
