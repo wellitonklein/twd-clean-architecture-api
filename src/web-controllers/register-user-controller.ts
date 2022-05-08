94e0cb1ab856ffa90ca7b9a9d3bcf85f2df45e31
Welliton Klein <welliton.fokushima@gmail.com>
2022-05-08 14:04:36 -0300

    refactor: remove unused conditional

--- src/web-controllers/register-user-controller.ts
+++ src/web-controllers/register-user-controller.ts
@@ -33,9 +33,7 @@ export class RegisterUserController {
         return badRequest(response.value)
       }
 
-      if (response.isRight()) {
-        return created(response.value)
-      }
+      return created(response.value)
     } catch (error) {
       return serverError(error)
     }
