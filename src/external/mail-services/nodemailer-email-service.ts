7ba5123c756e966e268e11f81fb82a1f56fb091b
Welliton Klein <welliton.fokushima@gmail.com>
2022-05-09 18:08:31 -0300

    feat: nodemailer adapter

--- src/external/mail-services/nodemailer-email-service.ts
+++ src/external/mail-services/nodemailer-email-service.ts
@@ -1,29 +1,33 @@
 import * as nodemailer from 'nodemailer'
 
-import { Either, right } from '@/shared'
+import { Either, left, right } from '@/shared'
 import { MailServiceError } from '@/usecases/errors'
 import { EmailOptions, EmailService } from '@/usecases/send-email/ports'
 
 export class NodeMailerEmailService implements EmailService {
   async send (options: EmailOptions): Promise<Either<MailServiceError, EmailOptions>> {
-    const transporter = nodemailer.createTransport({
-      host: options.host,
-      port: options.port,
-      auth: {
-        user: options.username,
-        pass: options.password
-      }
-    })
+    try {
+      const transporter = nodemailer.createTransport({
+        host: options.host,
+        port: options.port,
+        auth: {
+          user: options.username,
+          pass: options.password
+        }
+      })
 
-    await transporter.sendMail({
-      from: options.from,
-      to: options.to,
-      subject: options.subject,
-      text: options.text,
-      html: options.html,
-      attachments: options.attachments
-    })
+      await transporter.sendMail({
+        from: options.from,
+        to: options.to,
+        subject: options.subject,
+        text: options.text,
+        html: options.html,
+        attachments: options.attachments
+      })
 
-    return right(options)
+      return right(options)
+    } catch (error) {
+      return left(new MailServiceError())
+    }
   }
 }
