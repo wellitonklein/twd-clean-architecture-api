f9333ac920d3d696f3c45fe4685e1b12e00f8866
Welliton Klein <welliton.fokushima@gmail.com>
2022-05-09 16:34:23 -0300

    test: make use case work with users

--- test/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
+++ test/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -1,4 +1,4 @@
-import { UserData } from '@/entities'
+import { User, UserData } from '@/entities'
 import { UserRepository } from '@/usecases/register-user-on-mailing-list/ports'
 import { RegisterUserOnMailingList } from '@/usecases/register-user-on-mailing-list'
 import { InMemoryUserRepository } from '@/usecases/register-user-on-mailing-list/repository'
@@ -8,35 +8,12 @@ describe('Register user on mailing list use case', () => {
     const users: UserData[] = []
     const repo: UserRepository = new InMemoryUserRepository(users)
     const usecase = new RegisterUserOnMailingList(repo)
-    const name = 'any_name'
+    const name = 'any name'
     const email = 'any@email.com'
-    const response = await usecase.perform({ name, email })
-    const user = await repo.findUserByEmail(email)
-    expect(user.name).toBe(name)
-    expect(response.value.name).toBe(name)
-  })
-
-  test('should not add user with invalid email to mailing list', async () => {
-    const users: UserData[] = []
-    const repo: UserRepository = new InMemoryUserRepository(users)
-    const usecase = new RegisterUserOnMailingList(repo)
-    const name = 'any_name'
-    const invalidEmail = 'invalid_email.com'
-    const response = (await usecase.perform({ name, email: invalidEmail })).value as Error
-    const user = await repo.findUserByEmail(invalidEmail)
-    expect(user).toBeNull()
-    expect(response.name).toEqual('InvalidEmailError')
-  })
-
-  test('should not add user with invalid name to mailing list', async () => {
-    const users: UserData[] = []
-    const repo: UserRepository = new InMemoryUserRepository(users)
-    const usecase = new RegisterUserOnMailingList(repo)
-    const invalidName = ''
-    const email = 'any@mail.com'
-    const response = (await usecase.perform({ name: invalidName, email })).value as Error
-    const user = await repo.findUserByEmail(email)
-    expect(user).toBeNull()
-    expect(response.name).toEqual('InvalidNameError')
+    const user = User.create({ name, email }).value as User
+    const response = await usecase.perform(user)
+    const addUser = await repo.findUserByEmail(email)
+    expect(addUser.name).toBe(name)
+    expect(response.name).toBe(name)
   })
 })
