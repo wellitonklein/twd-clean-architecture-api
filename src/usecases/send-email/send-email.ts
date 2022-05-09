ac9981836df8d0702e99483608bf0c481bda61bc
Welliton Klein <welliton.fokushima@gmail.com>
2022-05-09 14:33:44 -0300

    feat: handle invalid user data

--- src/usecases/send-email/send-email.ts
+++ src/usecases/send-email/send-email.ts
@@ -1,20 +1,38 @@
-import { Either } from '@/shared'
+import { User, UserData } from '@/entities'
+import { InvalidEmailError, InvalidNameError } from '@/entities/errors'
+import { Either, left } from '@/shared'
 import { MailServiceError } from '@/usecases/errors'
 import { UseCase } from '@/usecases/ports'
 import { EmailOptions, EmailService } from '@/usecases/send-email/ports'
 
-export class SendEmail implements UseCase<EmailOptions, Either<MailServiceError, EmailOptions>> {
+export interface SendEmailParam {
+  options: EmailOptions
+  service: EmailService
+}
+
+export class SendEmail implements UseCase<UserData, Either<MailServiceError | InvalidNameError | InvalidEmailError, EmailOptions>> {
+  private readonly options: EmailOptions
   private readonly service: EmailService
 
-  constructor (service: EmailService) {
+  constructor ({ options, service }: SendEmailParam) {
+    this.options = options
     this.service = service
   }
 
-  async perform (request: EmailOptions): Promise<Either<MailServiceError, EmailOptions>> {
-    const greetings = `E aí <b>${request.username}</b>, beleza?`
-    const customizedHtml = `${greetings} </br></br> ${request.html}`
+  async perform (request: UserData): Promise<Either<MailServiceError | InvalidNameError | InvalidEmailError, EmailOptions>> {
+    const errorOrUser = User.create(request)
+
+    if (errorOrUser.isLeft()) {
+      return left(errorOrUser.value)
+    }
+
+    const user = errorOrUser.value as User
+
+    const greetings = `E aí <b>${user.name.value}</b>, beleza?`
+    const customizedHtml = `${greetings} </br></br> ${this.options.html}`
     const emailInfo: EmailOptions = {
-      ...request,
+      ...this.options,
+      to: `${user.name.value} <${user.email.value}>`,
       html: customizedHtml
     }
 
