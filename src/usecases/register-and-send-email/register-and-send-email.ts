f1ac28fbae6d9fa1c12c3314e12824b018ffedef
Welliton Klein <welliton.fokushima@gmail.com>
2022-05-09 15:30:32 -0300

    feat: register and send email

---
+++ src/usecases/register-and-send-email/register-and-send-email.ts
@@ -0,0 +1,46 @@
+import { RegisterUserOnMailingList } from '@/usecases/register-user-on-mailing-list'
+import { SendEmail } from '@/usecases/send-email'
+import { UseCase } from '@/usecases/ports'
+import { User, UserData } from '@/entities'
+import { Either, left, right } from '@/shared'
+import { InvalidEmailError, InvalidNameError } from '@/entities/errors'
+import { MailServiceError } from '@/usecases/errors'
+
+interface RegisterAndSendEmailParam {
+  registerUserOnMailingList: RegisterUserOnMailingList
+  sendEmail: SendEmail
+}
+
+export class RegisterAndSendEmail implements UseCase<UserData, Either<MailServiceError | InvalidNameError | InvalidEmailError, User>> {
+  private registerUserOnMailingList: RegisterUserOnMailingList
+  private sendEmail: SendEmail
+
+  constructor ({ registerUserOnMailingList, sendEmail }: RegisterAndSendEmailParam) {
+    this.registerUserOnMailingList = registerUserOnMailingList
+    this.sendEmail = sendEmail
+  }
+
+  async perform (request: UserData): Promise<Either<MailServiceError | InvalidNameError | InvalidEmailError, User>> {
+    const errorOrUser = User.create(request)
+
+    if (errorOrUser.isLeft()) {
+      return left(errorOrUser.value)
+    }
+
+    const user = errorOrUser.value as User
+
+    const userData: UserData = {
+      name: user.name.value,
+      email: user.email.value
+    }
+
+    await this.registerUserOnMailingList.perform(userData)
+    const response = await this.sendEmail.perform(userData)
+
+    if (response.isLeft()) {
+      return left(response.value)
+    }
+
+    return right(user)
+  }
+}
